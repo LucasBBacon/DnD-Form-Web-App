7f8615bdde25e5fa1355609f3764f75a00333824
LucasBBacon <lucasbrumatti@hotmail.com>
2026-05-11 17:30:56 +0100

    refactor: remove unused styles from FeatureCard and TabBar components; add new CSS files for styling

--- dnd-form-manager/src/components/RoleplayBoard/ui/FeatureCard.tsx
+++ dnd-form-manager/src/components/RoleplayBoard/ui/FeatureCard.tsx
@@ -1,4 +1,5 @@
 import type React from "react";
+import "./FeatureCard.css"
 
 export interface FeatureSource {
   key: string;
--- dnd-form-manager/src/components/RoleplayBoard/ui/TabBar.tsx
+++ dnd-form-manager/src/components/RoleplayBoard/ui/TabBar.tsx
@@ -1,4 +1,5 @@
 import type React from "react";
+import "./TabBar.css"
 
 export interface Tab {
   id: string;
